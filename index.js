1ccce250f2107db4c134cbd9d64a226c81b00cfa
Natali Nacinovic <natali.nacinovic@marfeel.com>
2018-10-20 15:16:04 +0200

    testing sensor data

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ const drawScreen = () => {
 };
 
 // Refresh the screen every 2 seconds
-let daysInterval = setInterval(drawScreen, 2000);
+//let daysInterval = setInterval(drawScreen, 2000);
 
 senseHat.Joystick.getJoystick().then(joystick => {
     joystick.on("press", direction => {
@@ -123,4 +123,24 @@ senseHat.Joystick.getJoystick().then(joystick => {
                 break;
         }
     });
+});
+
+const imu = require("node-sense-hat").Imu;
+
+const IMU = new imu.IMU();
+
+IMU.getValue((err, data) => {
+    if (err !== null) {
+        console.error("Could not read sensor data: ", err);
+        return;
+    }
+
+    console.log("Accelleration is: ", JSON.stringify(data.accel, null, "  "));
+    console.log("Gyroscope is: ", JSON.stringify(data.gyro, null, "  "));
+    console.log("Compass is: ", JSON.stringify(data.compass, null, "  "));
+    console.log("Fusion data is: ", JSON.stringify(data.fusionPose, null, "  "));
+
+    console.log("Temp is: ", data.temperature);
+    console.log("Pressure is: ", data.pressure);
+    console.log("Humidity is: ", data.humidity);
 });
